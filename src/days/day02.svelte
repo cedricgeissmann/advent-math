a860f16db0cb1a15997c4953ae5b4659a9eda0f8
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-12-02 09:46:42 +0100

    Day 2.

---
+++ src/days/day02.svelte
@@ -0,0 +1,34 @@
+<script>
+    import katex from "katex"
+    const f = katex.renderToString('f(x) = \\frac{1}{6} \\cdot x',
+        {displayMode: true})
+    const g = katex.renderToString('g(x) = -2 \\cdot x + 26')
+    const ans = katex.renderToString('S = (2, 12)')
+</script>
+
+<p>
+    Diese beiden Funktionen haben einen gemeinsamen Punkt. Bestimmen Sie
+diesen.
+</p>
+<p>{@html f}</p>
+<p>{@html g}</p>
+<div class="answer">Antwort: <span>{@html ans}</span></div>
+
+<style>
+
+.answer {
+    margin-top: 5rem;
+}
+
+.answer > span {
+    opacity: 0;
+    transition: opacity 0.6s ease;
+}
+
+.answer:hover > span,
+.answer:focus > span {
+    opacity: 1;
+}
+
+</style>
+
--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,24 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 function generateAlias(absolutePath) {
-    // eslint-disable-next-line no-undef
-    return path.resolve(process.cwd(), absolutePath);
+  // eslint-disable-next-line no-undef
+  return path.resolve(process.cwd(), absolutePath);
 }
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
-    base: "/advent-math/",
-    build: {
-        outDir: "dist",
+  plugins: [svelte()],
+  base: "/advent-math/",
+  build: {
+    outDir: "docs",
+  },
+  resolve: {
+    alias: {
+      "@": generateAlias("/"),
+      "@components": generateAlias("/src/components/"),
+      "@pages": generateAlias("/src/pages/"),
+      "@img": generateAlias("/img/"),
+      "@pdfs": generateAlias("/pdfs/"),
     },
-    resolve: {
-        alias: {
-            "@": generateAlias("/"),
-            "@components": generateAlias("/src/components/"),
-            "@pages": generateAlias("/src/pages/"),
-            "@img": generateAlias("/img/"),
-            "@pdfs": generateAlias("/pdfs/"),
-        },
-    },
+  },
 });
