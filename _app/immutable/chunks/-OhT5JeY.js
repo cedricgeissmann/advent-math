9dc8fc79119f52a47da3b0de7cb32537c3df2138
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-12-03 09:46:00 +0100

    Updates

---
+++ _app/immutable/chunks/-OhT5JeY.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_136bgs1?.base??"/advent-math",a=globalThis.__sveltekit_136bgs1?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/Dv4d9_Tx.js
@@ -0,0 +1,1 @@
+import{v as a}from"./BVbRF-ON.js";a();
